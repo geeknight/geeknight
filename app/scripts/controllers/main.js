6f8d26d798a7ee77723e69346ef71002e9eb8e79
manojlds <manojlds@gmail.com>
2013-12-06 11:13:13 +0530

    Cleanup main ctrl

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,12 +2,6 @@
 
 angular.module('geeknightApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
     $scope.tabs = ["When", "Where", "What"]
     $scope.selectedTabIndex = 0;
     $scope.when = true;
