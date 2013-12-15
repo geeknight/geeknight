f6a01ef08312091494f27f8bd8bd54785fac0bc6
SanjanaBayya <sanjanb@thoughtworks.com>
2013-12-15 20:14:49 +0530

    Sanjana | adding a participant details to db.

--- app/scripts/controllers/participate.js
+++ app/scripts/controllers/participate.js
@@ -1,9 +1,29 @@
 'use strict';
 
-angular.module('geeknightApp').controller('ParticipateCtrl', function ($scope, userService) {
-    $scope.user= userService.user;
+angular.module('geeknightApp').controller('ParticipateCtrl', function ($scope, userService, angularFire) {
+  var url = "https://geeknight.firebaseio.com/chennai/users";
+  var userBase = new Firebase(url);
+  $scope.isRegistered = false;
+  $scope.participants = [];
 
-    $scope.$on('userLogin', function() {
-        $scope.user = userService.user;
-    });
+  $scope.user = userService.user;
+
+  $scope.$on('userLogin', function () {
+    $scope.user = userService.user;
+  });
+
+  $scope.participate = function () {
+    $scope.participants.push(buildUser($scope.user));
+    $scope.isRegistered = true;
+    angularFire(userBase, $scope, 'participants');
+  };
+
+  var buildUser = function (user) {
+    return {
+      name: user.name,
+      phone: user.phone,
+      company: user.company,
+      email: user.email
+    }
+  }
 });
