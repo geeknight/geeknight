373aa3e8eb44a0da0aa75dedfef1583cde3116f1
Praveenram <praveenram.ba@gmail.com>
2013-12-28 15:35:17 +0530

    Fixed the hiding of form when registering participant issue, made registration unique

--- app/scripts/controllers/participate.js
+++ app/scripts/controllers/participate.js
@@ -3,21 +3,49 @@
 angular.module('geeknightApp').controller('ParticipateCtrl', function ($scope, userService, angularFire, edition) {
   var url = 'https://geeknight.firebaseio.com/chennai/Edition '+edition+'/participants';
   var userBase = new Firebase(url);
-  $scope.isRegistered = false;
   $scope.participants = angularFire(userBase, $scope, 'participants');
-
+  $scope.isRegistered = false;
   $scope.user = userService.user;
 
   $scope.$on('userLogin', function () {
     $scope.user = userService.user;
+    if($scope.user) {
+      $scope.isRegistered = isAlreadyRegistered();
+    }
   });
 
-  $scope.participate = function () {
-    _.any($scope.participants, function () {
+  $scope.newRegistration = function () {
+    $scope.user = null;
+    $scope.newParticipant = null;
+    $scope.isRegistered = false;
+  };
 
-    });
-    $scope.participants.push($scope.user);
+  $scope.participate = function () {
+    var isRegistered = isAlreadyRegistered();
+    if (!isRegistered) {
+      $scope.newParticipant = buildParticipant($scope.user);
+      angularFire(userBase, $scope, 'newParticipant');
+    }
     $scope.isRegistered = true;
-    angularFire(userBase, $scope, 'participants');
+  };
+
+  var buildParticipant = function (user) {
+    return [{
+      name: user.name,
+      phone: user.phone,
+      email: user.email,
+      company: user.company
+    }];
+  };
+
+  var isAlreadyRegistered = function () {
+    var properties = ['company','email','phone','name'];
+    return _.any($scope.participants, function (participant) {
+      var alreadyRegistered = true;
+      _.each(properties, function (property) {
+        alreadyRegistered = alreadyRegistered && ($scope.user[property].toLowerCase() === participant[property].toLowerCase());
+      });
+      return alreadyRegistered;
+    });
   };
 });
